c823aaf60ddcfae5bc742b584e3d8ed13a7acb34
Your Name <you@example.com>
2026-07-08 10:08:05 +0900

    On branch main  Your branch is up to date with 'origin/main'.
     feat: create tools
      - hashgen
      - keygen
     update: readme
     add: example .env
    
     Changes to be committed:
    	new file:   .env _example
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   README.md
    	modified:   src/auth.rs
    	new file:   tools/hashgen/Cargo.toml
    	new file:   tools/hashgen/src/main.rs
    	new file:   tools/keygen/Cargo.toml
    	new file:   tools/keygen/src/main.rs

--- src/auth.rs
+++ src/auth.rs
@@ -2,6 +2,11 @@ use crate::config::Config;
 use bcrypt::verify;
 
 pub fn authenticate(username: &str, password: &str, config: &Config) -> Option<UserRole> {
+    // println!("Input username: {}", username);
+    // println!("Input password: {}", password);
+    // println!("Stored user hash: {}", &config.user_password_hash);
+    // println!("Stored admin hash: {}", &config.admin_password_hash);
+
     match username {
         u if u == config.user_name => {
             if verify(password, &config.user_password_hash).ok()? {
